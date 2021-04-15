e24de24a4eee07d728d7b4d303f721a22df2185c
Ummam-png <82569010+Ummam-png@users.noreply.github.com>
2021-04-15 16:08:15 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/bash
 
-apt-get update
-apt-get upgrade
-apt-get install nodejs
-apt-get install libwebp
-apt-get install ffmpeg
-apt-get install wget
-apt-get install tesseract
+apt-get update -y
+apt-get upgrade -y
+apt-get install nodejs -y
+apt-get install libwebp -y
+apt-get install mc -y
+apt-get install ffmpeg -y
+apt-get install wget -y
+apt-get install tesseract -y
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm i imgbb-uploader
 npm install
 
-echo "[*] All dependencies have been install, please run the command \"npm start\" to immediately start the script"
+echo "[❗] SILAKAN KETIK  [node index.js] UNTUK MENJALANKAN SCRIPT"
